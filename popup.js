d70c685d6f1c8e7344f6f8667217188deeee19fc
Abhinav Prakash <abprakash85409@gmail.com>
2024-12-02 17:23:49 +0530

    Feat -> auto login Toggle button

--- popup.js
+++ popup.js
@@ -1,20 +1,31 @@
-document.getElementById('save').addEventListener('click', function() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    chrome.runtime.sendMessage(
-      {
-        action: 'saveCredentials',
-        credentials: { username, password }
-      },
-      function(response) {
-        if (response.status === "success") {
-          alert('Credentials Saved!');
-        } else {
-          alert('Error saving credentials');
-        }
+
+document.addEventListener('DOMContentLoaded', function () {
+  const toggle = document.getElementById('auto-login-toggle');
+
+  chrome.storage.sync.get(['autoLoginEnabled'], function (data) {
+      if (data.autoLoginEnabled) {
+          toggle.checked = true;
+      } else {
+          toggle.checked = false;
       }
-    );
   });
-  
 
 
+  toggle.addEventListener('change', function () {
+      const isChecked = toggle.checked;
+      chrome.storage.sync.set({ autoLoginEnabled: isChecked });
+    
+  });
+
+
+  const form = document.getElementById('credentials-form');
+  form.addEventListener('submit', function (e) {
+      e.preventDefault();  // Prevent form submission
+      const username = document.getElementById('username').value;
+      const password = document.getElementById('password').value;
+      
+      chrome.storage.sync.set({ username, password }, function () {
+          alert('Credentials saved!');
+      });
+  });
+});
